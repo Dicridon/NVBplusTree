2a4ca89e2c0e4e1e4c830286c1d3504a1b4fdcd7
Dicridon <noahxiong@outlook.com>
2019-03-04 17:10:19 +0800

    list done, have not been tested

--- list.c
+++ list.c
@@ -1,49 +1,129 @@
 #include "list.h"
-list_node_t *new_list_node() {
-    list_node_t *node = (list_node_t *)malloc(sizeof(list_node_t));
-    node->next = node;
-    node->prev = node;
-    return node; // NULL is OK.
+
+int
+list_node_constr(PMEMobjpool *pop, void *ptr, void *arg)
+{
+    TOID(struct list_node) *node = ptr;
+    TOID(struct list_node) *prev = &((struct list_node *)arg)->prev;
+    TOID(struct list_node) *next = &((struct list_node *)arg)->next;
+    
+    list_node_t *node_ptr = D_RW(*node);
+    list_node_t *prev_ptr = D_RW(*prev);
+    list_node_t *next_ptr = D_RW(*next);
+    
+    node_ptr->prev = *prev;
+    node_ptr->next = *next;
+    prev_ptr->next = *node;
+    next_ptr->prev = *node;
+
+    pmemobj_persist(pop, node, sizeof(TOID(struct list_node)));
+    pmemobj_persist(pop, prev, sizeof(TOID(struct list_node)));
+    pmemobj_persist(pop, next, sizeof(TOID(struct list_node)));
+    return 0;
 }
 
-list_t *new_list() {
-    list_t *list = (list_t *)malloc(sizeof(list_t));
-    list->head = (list_node_t *)malloc(sizeof(list_node_t));
-    list->head->next = list->head;
-    list->head->prev = list->head;
-    return list;
+static int
+list_head_constr(PMEMobjpool *pop, void *ptr, void *arg)
+{
+    // just to silence warnings
+    void *temp = arg;
+    arg = temp;
+
+    
+    TOID(struct list_node) *head = ptr;
+    list_node_t *head_ptr = D_RW(*head);
+    head_ptr->next = *head;
+    head_ptr->prev = *head;
+    pmemobj_persist(pop, head, sizeof(TOID(struct list_node)));
+    return 0;
 }
 
-void list_add_to_head(list_t *list, list_node_t *node) {
-    node->next = list->head->next;
-    node->prev = list->head;
-    list->head->next = node;
+
+// the address of root object may vary due to the change of size
+// be cautious when storing a pointer pointing to this root in other objects
+int
+new_list(PMEMobjpool *pop, TOID(struct list) *list)
+{
+    // POBJ_ROOT will allocate space or retrive exsiting root
+    *list = POBJ_ROOT(pop, struct list);
+    list_t *list_ptr = D_RW(*list);
+    list_head_constr(pop, &list_ptr->head, NULL);
+    // no need to invode persist on list
+    return 1;
 }
 
-void list_add(list_node_t *ex, list_node_t *node) {
-    node->next = ex->next;
-    node->prev = ex;
-    ex->next->prev = node;
-    ex->next = node;
+void
+list_add_to_head(PMEMobjpool *pop,
+                 TOID(struct list) *list, TOID(struct list_node) *node)
+{
+    list_t *list_ptr = D_RW(*list);
+    
+    TOID(struct list_node) *head = &list_ptr->head;
+    list_node_t *head_ptr = D_RW(*head);
+
+    TOID(struct list_node) *next = &head_ptr->next;
+
+    list_node_t arg = {
+        .prev = *head,
+        .next = *next
+    };
+
+    POBJ_ALLOC(pop, node,
+               struct list_node, sizeof(list_node_t), list_node_constr, &arg);
 }
 
-void list_remove(list_node_t *node) {
-    node->prev->next = node->next;
-    node->next->prev = node->prev;
+void
+list_add(PMEMobjpool *pop,
+         TOID(struct list_node) *prev, TOID(struct list_node) *node) {
+    list_node_t *prev_ptr = D_RW(*prev);
+    
+    TOID(struct list_node) *next = &prev_ptr->next;
+    
+    list_node_t arg = {
+        .prev = *prev,
+        .next = *next
+    };
+
+    POBJ_ALLOC(pop, node,
+               struct list_node, sizeof(list_node_t), list_node_constr, &arg);
 }
 
-void free_list_node(list_node_t *n) {
-    free(n);
+void
+list_remove(PMEMobjpool *pop, TOID(struct list_node) *node) {
+    list_node_t *node_ptr = D_RW(*node);
+
+    TOID(struct list_node) *prev = &node_ptr->prev;
+    list_node_t *prev_ptr = D_RW(*prev);
+    
+    TOID(struct list_node) *next = &node_ptr->next;
+    list_node_t *next_ptr = D_RW(*next);
+
+    prev_ptr->next = *next;
+    next_ptr->prev = *prev;
+    POBJ_FREE(node);
+    pmemobj_persist(pop, prev, sizeof(TOID(struct list_node)));
+    pmemobj_persist(pop, next, sizeof(TOID(struct list_node)));
 }
 
-void list_destroy(list_t *t) {
-    list_node_t *p = t->head->next;
-    while(p != t->head) {
-        t->head->next = p->next;
-        free(p);
-        p = t->head->next;
+void
+list_destroy(TOID(struct list) *list) {
+    list_t *list_ptr = D_RW(*list);
+
+    TOID(struct list_node) *head = &list_ptr->head;
+    list_node_t *head_ptr = D_RW(*head);
+    TOID(struct list_node) *walk = &head_ptr->next;
+    list_node_t *walk_ptr = D_RW(*walk);
+
+    while (walk != head) {
+        head_ptr->next = walk_ptr->next;
+        POBJ_FREE(walk);
+        walk = &head_ptr->next;
+        walk_ptr = D_RW(*walk);
     }
-    free(t->head);
-    free(t);
-    t = NULL;
+    POBJ_FREE(list);
+}
+
+void
+free_list_node(TOID(struct list_node) *node) {
+    POBJ_FREE(node);
 }
--- list.h
+++ list.h
@@ -1,27 +1,47 @@
 #ifndef __FROST_LIST__
 #define __FROST_LIST__
 #include <stdlib.h>
+#include <libpmemobj.h>
+
+POBJ_LAYOUT_BEGIN(dlist);
+POBJ_LAYOUT_ROOT(dlist, struct list);
+POBJ_LAYOUT_TOID(dlist, struct list_node);
+POBJ_LAYOUT_END(dlist);
 
 typedef struct list_node {
-    struct list_node *prev;
-    struct list_node *next;
+    TOID(struct list_node) prev;
+    TOID(struct list_node) next;
 } list_node_t;
 
 typedef struct list {
-    list_node_t *head;
+    TOID(struct list_node) head;
 } list_t;
 
-list_node_t *new_list_node();
 
-list_t *new_list();
+// I should try to hide those ugly interfaces in PMDk
 
-void list_add_to_head(list_t *list, list_node_t *node);
+// void *ptr should actually be TOID(struct list_node *) type
+int
+list_node_constr(PMEMobjpool *pop, void *ptr, void *arg);
 
-void list_add(list_node_t *ex, list_node_t *node) ;
 
-void list_remove(list_node_t *node);
+int
+new_list(PMEMobjpool *pop, TOID(struct list) *list);
 
-void list_destroy(list_t *t);
+void
+list_add_to_head(PMEMobjpool *pop,
+                 TOID(struct list) *list, TOID(struct list_node) *node);
 
-void free_list_node(list_node_t *l);
+void
+list_add(PMEMobjpool *pop,
+         TOID(struct list_node) *prev, TOID(struct list_node) *node) ;
+
+void
+list_remove(PMEMobjpool *pop, TOID(struct list_node) *node);
+
+void
+list_destroy(TOID(struct list) *l);
+
+void
+free_list_node(TOID(struct list_node) *node);
 #endif
